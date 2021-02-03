798b905915781646a2184b6c6d33029536061681
Giang Nguyen <gn02.nguyen@gmail.com>
2021-02-03 11:57:01 -0800

    Implemented location manager.

--- Appa/Views/ContentView.swift
+++ Appa/Views/ContentView.swift
@@ -10,8 +10,25 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    
+    @ObservedObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
 
     var body: some View {
-        MapView()
+        //MapView()
+        VStack {
+            Text("location status: \(locationManager.statusString)")
+            HStack {
+                Text("latitude: \(userLatitude)")
+                Text("longtidude: \(userLongitude)")
+            }
+        }
     }
 }
